8b8604812e2573747d89208d85792596cfd6b5ff
hayley jolliffe <93291089+hayleyjolliffe@users.noreply.github.com>
2022-01-24 16:13:36 -0500

    Created calculate.php for Lab 4

---
+++ calculate.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en-Us">
+
+<head>
+<meta charset="utf-8">
+<title>Product Shipping Calculator</title>
+<style type="text/css">
+    .number {
+        font-weight:bold;
+        color: red;
+    }
+</style>
+</head>
+
+<body>
+<?php
+/* This script takes values from calculator.html
+and calculates the total cost and monthly payments */
+
+// Get the values from the $_POST array
+$price = $_POST['price'];
+$quantity = $_POST['quantity'];
+$discount = $_POST['discount'];
+$tax = $_POST['tax'];
+$shipping = $_POST['shipping'];
+$payments = $_POST['payments'];
+
+// Calculate the total
+$total = $price * $quantity;
+$total = $total + $shipping;
+$total = $total = $discount;
+
+// Determine the tax rate
+$taxrate = $tax / 100;
+$taxrate = $taxrate + 1;
+
+// Factor in the tax rate
+$total = $total * $taxrate;
+
+// Calculate the monthly payments
+$monthly = $total / $payments;
+
+// Print out the results
+print "<p>You have selected to purchase:<br />
+<span class=\"number\">$quantity widget(s) at $<span class=\"number\">$price</span> each.
+The shipping cost is $<span class=\"number\">$shipping</span>. <br />
+You have selected a <span class=\"number\">$tax%</span> tax rate.<br />
+
+After your $<span class=\"number\">$discount</span> discount, the total cost is
+$<span class=\"number\">$total</span>.<br />
+
+Divided over <span class=\"number\">$payments</span> monthly payments, it comes to
+$<span class=\"number\">$monthly</span> each month.</p>";
+
+?>
+
+</body>
+</html>
