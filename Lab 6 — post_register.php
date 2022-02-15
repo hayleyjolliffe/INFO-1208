135b24d87871b05c735d90e1c177e0c412f440af
hayley jolliffe <93291089+hayleyjolliffe@users.noreply.github.com>
2022-02-15 12:07:16 -0500

    Created 'post_register.php' for Lab 6

---
+++ Lab 6 — post_register.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <title>Hayley Jolliffe Registration</title>
+
+</head>
+
+<body>
+<?php
+// This script receives data from register.html
+print "<h1>Registration Confirmation</h1>";
+// Initialize the flag variable
+$okay = true;
+// If there are no errors, print a confirmation message
+if ($okay) {
+    print "Success! Thank you for registering.";
+}
+?>
+</body>
+</html>
