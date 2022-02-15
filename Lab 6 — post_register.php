df63cbec3ac0db22f5551d69f467ee14b654aba8
hayley jolliffe <93291089+hayleyjolliffe@users.noreply.github.com>
2022-02-15 12:32:47 -0500

    Updated 'post_register.php'

--- Lab 6 — post_register.php
+++ Lab 6 — post_register.php
@@ -4,6 +4,11 @@
 
     <meta charset="utf-8">
     <title>Hayley Jolliffe Registration</title>
+    <style type="text/css" media="screen">
+        .error {
+            color:red;
+        }
+    </style>
 
 </head>
 
@@ -13,6 +18,16 @@
 print "<h1>Registration Confirmation</h1>";
 // Initialize the flag variable
 $okay = true;
+// Validate the email address
+if (empty($_POST['email'])) {
+    print '<p class="error"> Please enter your email address.</p>';
+    $okay = false;
+}
+// Validate the password address
+if (empty($_POST['password'])) {
+    print '<p class="error"> Please enter your password.</p>';
+    $okay = false;
+}
 // If there are no errors, print a confirmation message
 if ($okay) {
     print "Success! Thank you for registering.";
