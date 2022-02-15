295b16294e91e83b2af5fae19b2cc6f737e5ce9f
hayley jolliffe <93291089+hayleyjolliffe@users.noreply.github.com>
2022-02-15 12:33:37 -0500

    Update Lab 6 — post_register.php

--- Lab 6 — post_register.php
+++ Lab 6 — post_register.php
@@ -28,11 +28,22 @@ if (empty($_POST['password'])) {
     print '<p class="error"> Please enter your password.</p>';
     $okay = false;
 }
+// Check to see if the password entries match
+if ($_POST['password'] != $_POST['confirm']) {
+    print '<p class="error">Passwords do not match</p>';
+    $okay = false;
+}
 // Validate the year of birth
 if (is_numeric($_POST['year'])) {
     $age = date('Y') - $_POST['year']; // Calculate age by subtracting from the current year
 } else {
     print '<p class="error"> Please enter a valid year of birth.</p>';
+    $okay = false;
+}
+// Born this year or in the future? No way!
+// Note in date('Y'), it has to be an uppercase Y
+if ($_POST['year'] >= date('Y')) {
+    print '<p class="error">Double check your birth date!</p>';
     $okay = false;
 }
 // If there are no errors, print a confirmation message
