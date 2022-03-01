9b76d6fcb8509783e869807fbfa6789033fdee50
hayley jolliffe <93291089+hayleyjolliffe@users.noreply.github.com>
2022-03-01 17:03:17 -0500

    Updating PRINT

--- Lab 7 — months1.php
+++ Lab 7 — months1.php
@@ -26,7 +26,19 @@ $months = [
 'Nov',
 'Dec'
 ];
-print "<p>$months</p>";
+// Print the number of every month
+foreach ($months as $name => $month) {
+	print "<p>$month is month number: $name</p>\n";
+}
+// Print the current number of months
+$current = count($months);
+print "<p>There are $current months</p>";
+// Add 2 new months to the list
+$months[] = 'Winter';
+$months[] = 'Construction';
+// Print the new number of months
+$new = count($months);
+print "<p>Now there are $new months</p>";
 print_r ($months);
 ?>
     
