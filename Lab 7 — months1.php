fb21f8a563943127d8f0636d14617674c4ee2dec
hayley jolliffe <93291089+hayleyjolliffe@users.noreply.github.com>
2022-03-01 16:58:44 -0500

    Created 'months1.php' for PHP Lab 07

---
+++ Lab 7 — months1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <title>Hayley Jolliffe Array</title>
+
+</head>
+
+<body>
+
+<h1>List of months</h1>
+
+<?php
+$months = [
+1 => 'Jan',
+'Feb',
+'Mar',
+'Apr',
+'May',
+'Jun',
+'Jul',
+'Aug',
+'Sep',
+'Oct',
+'Nov',
+'Dec'
+];
+print "<p>$months</p>";
+print_r ($months);
+?>
+    
+
+</body> 
+</html>
