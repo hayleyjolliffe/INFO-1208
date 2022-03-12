16d244929cb9da0e9395cf93d30e2c54f5b0f6fe
hayley jolliffe <93291089+hayleyjolliffe@users.noreply.github.com>
2022-03-12 10:14:10 -0500

    Working 'login.php'

--- Lab 8 — login.php
+++ Lab 8 — login.php
@@ -4,28 +4,26 @@
 define('TITLE', 'User Login');
 include('templates/header.html');
 // Ask the user to login
-print '<h2>Login Form</h2>
-    <p>Log in to earn Super Duper loyalty points</p>';
+print '<h2>Login Form</h2><p>Log in to earn Super Duper loyalty points</p>';
 // Check if the form has been submitted
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Handle the form
     if ( (!empty($_POST['email'])) && (!empty($_POST['password'])) ) {
-        if( (strtolower($_POST['email']) == 'test@test.com') && ($_POST['password']) == 'test') ) { // Correct!
-            ob_end_clean(); //
+        if ( (strtolower($_POST['email']) == 'test@test.com') && ($_POST['password'] == 'test') ) {
+            ob_end_clean();
             header('Location: specials.php');
             exit();
-        } else { // Wrong user or password
+        } else {
             print '<p class="text--error">Email or Password does not match.<br />Go back and try again.</p>';
-        }   
-    } else { // Forgot a field
+        }
+    } else {
         print '<p class="text--error">Email address and password are required.<br />Go back and try again.</p>';
     }
-} else { // Display the form
+} else {
     print '<form action="login.php" method="post" class="form--inline">
     <p><label for="email">Email Address:</label><input type="email" name="email" size="20"></p>
     <p><label for="password">Password:</label><input type="password" name="password" size="20"></p>
-    <p><input type="submit" name="submit" value="Log In" class="button--pill"></p>
-    </form>';
+    <p><input type="submit" name="submit" value="Log In" class="button--pill"></p></form>';
 }
-include('templates/footer.html'); // Include the footer file
+include('templates/footer.html');
 ?>
