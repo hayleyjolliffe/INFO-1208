81dc0428711dcdee60fabfe521b556685d579b76
hayley jolliffe <93291089+hayleyjolliffe@users.noreply.github.com>
2022-03-19 16:33:29 -0400

    Update Lab 9 — customize.php

--- Lab 9 — customize.php
+++ Lab 9 — customize.php
@@ -22,3 +22,27 @@ if (isset($msg)) {
     print $msg:
 }
 ?>
+<p>Use this form to set preferences for FOLusername:</p>
+<form action="customize.php" method="post">
+    <select name="font_size">
+        <option value="">Font Size</option>
+        <option value="xx-small">xx-small</option>
+        <option value="x-small">x-small</option>
+        <option value="small">small</option>
+        <option value="medium">medium</option>
+        <option value="large">large</option>
+        <option value="x-large">x-large</option>
+        <option value="xx-large">xx-large</option>
+    </select>
+    <select name="font_color">
+        <option value="">Font Color</option>
+        <option value="999">Gray</option>
+        <option value="0c0">Green</option>
+        <option value="00f">Blue</option>
+        <option value="c00">Red</option>
+        <option value="000">Black</option>
+    </select>
+    <input type="submit" name="submit" value="Set My Preferences">
+</form>
+</body>
+</html>
